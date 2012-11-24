10fbd4dce52efa229f7d0df862c724725bf5509c
Arjun Patel <arjunap@gmail.com>
2012-11-24 15:30:06 -0800

    initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arqu::Application.config.secret_token = '44ac4d92cd8d0f99d3eefabd08efa1b0bf3b011a8ab7b9ccc472eaec486b693159dc5d2de2e91858838427ad238994c7b3acda09df644ad3991e34c0782d589e'
