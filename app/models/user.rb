0a47469fd8ca630d8d434bd3d276df9348bd2a89
Arjun Patel <arjunap@gmail.com>
2012-11-28 15:58:16 -0800

    added form helper in new and edit views

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :country,
-                  :city, :age, :gender, :native_language
+                  :state, :age, :gender, :native_language
   # attr_accessible :title, :body
 end
