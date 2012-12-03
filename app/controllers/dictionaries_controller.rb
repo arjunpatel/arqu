f6b95c1ab206fddaa7002d50bf3c49cd54e1d3ee
Quynh Nguyen <quynhnguyen003@gmail.com>
2012-12-02 21:15:25 -0800

    add regex to parse out id from voice file

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -1,6 +1,26 @@
 class DictionariesController < InheritedResources::Base
-	def new
-		## flash[:notice] = session["warden.user.user.key"][1][0] -> Devise user_id
-		
+
+	#Bypass token authenticity
+	skip_filter :verify_authenticity_token
+
+	def create
+		user_id = params[:user_id].to_i
+		word = params[:word]
+		voice_id = /http:\/\/vocaroo.com\/i\/(\w+)/.match(params[:voice_id])[1]
+		description = params[:description] || ""
+
+		##Fetch current loggedin user
+		if user_id > 0
+			user = User.find(user_id)
+			dict = Dictionary.create!(:word => word, :voice_id => voice_id, :description => description)
+			user.dictionaries << dict
+			user.save!
+			flash[:notice] = "Your voice file has been added to our database"
+		else
+			flash[:notice] = "Please register/login first!"
+		end
+
+		render :new
+
 	end
 end
