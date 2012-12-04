9d8d8b321ff60277ac7d4d5b0809716342661a1e
Quynh Nguyen <quynhnguyen003@gmail.com>
2012-12-04 10:05:19 -0800

    modify front page data display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-    render :layout => false
+    @unique_word= Dictionary.find_by_sql("SELECT * FROM dictionaries GROUP BY word")
+  	@unique_word_count = @unique_word.length
+  	@voice_count = Dictionary.all.length
+  	@country_count = User.find_by_sql("SELECT * FROM users GROUP BY country").length
+  	render :layout => false
   end
 
 end
