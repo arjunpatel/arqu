2b0121c39d7a7f9ed93db1687e3c77034ab0ddbf
Quynh Nguyen <quynhnguyen003@gmail.com>
2012-12-04 15:32:03 -0800

    change gender input box into dropdown menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   	@unique_word_count = @unique_word.length
   	@voice_count = Dictionary.all.length
   	@country_count = User.find_by_sql("SELECT * FROM users GROUP BY country").length
+  	@latest_feed = Dictionary.find(:all, :limit => 10)
+  	puts "DEBUG Xxxxxxxxxxxxxxxxxxx  #{@latest_feed}  Xxxxxxxxxxxxxxxxxxx"
   	render :layout => false
   end
 
