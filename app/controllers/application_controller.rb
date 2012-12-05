8d14359741c7bc63603cc65b9ea7f671310d32b8
Quynh Nguyen <quynhnguyen003@gmail.com>
2012-12-05 07:39:47 -0800

    fix database problem with heroku for latest feed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-    @unique_word= Dictionary.find_by_sql("SELECT * FROM dictionaries GROUP BY word")
-  	@unique_word_count = @unique_word.length
+  	@unique_word_count = Dictionary.count(:group => "word").count
   	@voice_count = Dictionary.all.length
-  	@country_count = User.find_by_sql("SELECT * FROM users GROUP BY country").length
-  	@latest_feed = Dictionary.find(:all, :limit => 10)
+  	@country_count = User.count(:group => "country").count
+    @latest_feed = Dictionary.find(:all, :limit => 10)
   	puts "DEBUG Xxxxxxxxxxxxxxxxxxx  #{@latest_feed}  Xxxxxxxxxxxxxxxxxxx"
   	render :layout => false
   end
